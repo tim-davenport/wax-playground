6e0408e8c49b0fdecc85df538d76c8eeedcc7f04
tim-davenport <timdavenport81@gmail.com>
2021-08-24 20:00:11 +0000

    add createcol action (not working)

--- index.js
+++ index.js
@@ -14,4 +14,30 @@ const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), te
 (async () => {
     res = await rpc.get_account('ohvanankaahu') //get alice's account info.  This assumes the account 'alice' has been created on the chain specified in the rpc object.
     console.dir(res);
+    
+    const result = await api.transact({
+        actions: [{
+          account: 'atomicassets',
+          name: 'createcol',
+          authorization: [{
+            actor: 'ohvanankaahu',
+            permission: 'owner',
+          }],
+          data: {
+            author: 'ohvanankaahu',
+            collection_name: 'testtesttest',
+            allow_notify: false,
+            authorized_accounts: 'ohvanankaahu',
+            notify_accounts: 'ohvanankaahu',
+            market_fee: 0.1,
+            data: [{"key": "id", "value": ["uint64", 1024]}, {"key": "color", "value": ["string", "pink"]}]
+          },
+        }]
+      }, {
+        blocksBehind: 3,
+        expireSeconds: 1200,
+      });
+
+    console.dir(result);
+
 })();

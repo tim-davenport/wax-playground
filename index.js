e3682f6a50d1a944a7169d90a2daaf4d0950b71d
tim-davenport <timdavenport81@gmail.com>
2021-08-27 10:40:57 +0000

    added mintasset and transfer actions

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { error } = require('console');
 const { Api, JsonRpc, RpcError } = require('eosjs');
 const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig');      // development only
 const fetch = require('node-fetch');                                    // node only; not needed in browsers
@@ -11,45 +12,201 @@ const rpc = new JsonRpc('https://testnet.waxsweden.org', { fetch });
 const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
 
 (async () => {
-    // res = await rpc.get_account('ohvanankaahu') //get alice's account info.  This assumes the account 'alice' has been created on the chain specified in the rpc object.
-    // console.dir(res);
+    await rpc.get_account('ohvanankaahu')
+      .then(res => {
+        console.log("Account information:");
+        console.dir(res);
+      }).catch(err => {
+        console.log("Failed to get account information");
+        console.dir(err.message);
+      });
     
-    // let result = await api.transact({
-    //     actions: [{
-    //       account: 'atomicassets',
-    //       name: 'createcol',
-    //       authorization: [{
-    //         actor: 'ohvanankaahu',
-    //         permission: 'owner',
-    //       }],
-    //       data: {
-    //         author: 'ohvanankaahu',
-    //         collection_name: 'dhtestdhtest',
-    //         allow_notify: false,
-    //         authorized_accounts: [],
-    //         notify_accounts: [],
-    //         market_fee: 0.1,
-    //         data: []
-    //       },
-    //     }]
-    //   }, {
-    //     blocksBehind: 3,
-    //     expireSeconds: 1200,
-    //   });
+    await api.transact({
+        actions: [{
+          account: 'atomicassets',
+          name: 'createcol',
+          authorization: [{
+            actor: 'ohvanankaahu',
+            permission: 'owner',
+          }],
+          data: {
+            author: 'ohvanankaahu',
+            collection_name: 'dhtestdhtest',
+            allow_notify: false,
+            authorized_accounts: [],
+            notify_accounts: [],
+            market_fee: 0.1,
+            data: []
+          },
+        }]
+      }, {
+        blocksBehind: 3,
+        expireSeconds: 1200,
+      }).then(res => {
+        console.dir(res);
+      }).catch(err => {
+        console.log("failed to create collection - see next line");
+        console.dir(err.message);
+        //console.dir(err);
+      });
 
-    // console.dir(result);
+      await api.transact({
+        actions: [{
+          account: 'atomicassets',
+          name: 'addcolauth',
+          authorization: [{
+            actor: 'ohvanankaahu',
+            permission: 'owner',
+          }],
+          data: {
+            account_to_add: 'ohvanankaahu',
+            collection_name: 'dhtestdhtest'
+          },
+        }]
+      }, {
+        blocksBehind: 3,
+        expireSeconds: 1200,
+      }).then(res => {
+        console.dir(res);
+      }).catch(err => {
+        console.log("failed to add authorised account to collection - see next line");
+        console.dir(err.message);
+        //console.dir(err);
+      });
 
-    result = await rpc.get_table_rows({
-      json: true,               // Get the response as json
-      code: 'atomicassets',      // Contract that we target
-      scope: 'ohvanankaahu',         // Account that owns the data
-      table: 'collections',        // Table name
-      limit: 10,                // Maximum number of rows that we want to get
-      reverse: false,           // Optional: Get reversed data
-      show_payer: false,          // Optional: Show ram payer
-      collection_name: "dhtestdhtest"
+      await api.transact({
+        actions: [{
+          account: 'atomicassets',
+          name: 'createschema',
+          authorization: [{
+            actor: 'ohvanankaahu',
+            permission: 'owner',
+          }],
+          data: {
+            authorized_creator: 'ohvanankaahu',
+            collection_name: 'dhtestdhtest',
+            schema_name: "dhschema",
+            schema_format: [{"name": "name", "type": "string"}, {"name": "tree_type", "type": "string"}]
+          },
+        }]
+      }, {
+        blocksBehind: 3,
+        expireSeconds: 1200,
+      }).then(res => {
+        console.dir(res);
+      }).catch(err => {
+        console.log("failed to create schema - see next line");
+        console.dir(err.message);
+        //console.dir(err);
+      });
+
+
+      //a82f7114d5906a9b13cdcef7514fca3977bf8abafbdf258c1ea2a62d19ca637f
+      //160836 (template id)
+      //above are the transaction Id and associated template Id - to create more templates run the below commented out section
+
+      // await api.transact({
+      //   actions: [{
+      //     account: 'atomicassets',
+      //     name: 'createtempl',
+      //     authorization: [{
+      //       actor: 'ohvanankaahu',
+      //       permission: 'owner',
+      //     }],
+      //     data: {
+      //       authorized_creator: 'ohvanankaahu',
+      //       collection_name: 'dhtestdhtest',
+      //       schema_name: "dhschema",
+      //       transferable: true,
+      //       burnable: true,
+      //       max_supply: 0,
+      //       immutable_data: [{"key": "name", "value": ["string", "Test Token"]}]
+      //     },
+      //   }]
+      // }, {
+      //   blocksBehind: 3,
+      //   expireSeconds: 1200,
+      // }).then(res => {
+      //   console.dir(res);
+      // }).catch(err => {
+      //   console.log("failed to create template - see next line");
+      //   console.dir(err.message);
+      //   //console.dir(err);
+      // });
+
+      // await api.transact({
+      //   actions: [{
+      //     account: 'atomicassets',
+      //     name: 'mintasset',
+      //     authorization: [{
+      //       actor: 'ohvanankaahu',
+      //       permission: 'owner',
+      //     }],
+      //     data: {
+      //       authorized_minter: 'ohvanankaahu',
+      //       collection_name: 'dhtestdhtest',
+      //       schema_name: "dhschema",
+      //       template_id: 160836,
+      //       new_asset_owner: 'ohvanankaahu',
+      //       immutable_data: [{"key": "tree_type", "value": ["string", "Silver Birch"]}],
+      //       mutable_data: [],
+      //       tokens_to_back: []
+      //     },
+      //   }]
+      // }, {
+      //   blocksBehind: 3,
+      //   expireSeconds: 1200,
+      // }).then(res => {
+      //   console.dir(res);
+      // }).catch(err => {
+      //   console.log("failed to mint asset - see next line");
+      //   console.dir(err.message);
+      //   //console.dir(err);
+      // });
+
+    //TRANSER ASSET
+
+    await api.transact({
+      actions: [{
+        account: 'atomicassets',
+        name: 'transfer',
+        authorization: [{
+          actor: 'ohvanankaahu',
+          permission: 'owner',
+        }],
+        data: {
+          from: 'ohvanankaahu',
+          to: 'test1111test',
+          asset_ids: [1099517092989],
+          memo: "testing 123"
+        },
+      }]
+    }, {
+      blocksBehind: 3,
+      expireSeconds: 1200,
+    }).then(res => {
+      console.dir(res);
+    }).catch(err => {
+      console.log("failed to transer asset - see next line");
+      console.dir(err.message);
+      //console.dir(err);
     });
 
-    console.dir(result);
+    // await rpc.get_table_rows({
+    //   json: true,               // Get the response as json
+    //   code: 'atomicassets',      // Contract that we target
+    //   scope: 'afrasvwertca',         // Account that owns the data
+    //   table: 'assets',        // Table name
+    //   limit: 10,                // Maximum number of rows that we want to get
+    //   reverse: false,           // Optional: Get reversed data
+    //   show_payer: false,          // Optional: Show ram payer
+    //   owner: "afrasvwertca"
+    // }).then(res => {
+    //   console.dir(res);
+    // }).catch(err => {
+    //   console.dir(err);
+    // });
+
+    // console.dir(result);
 
 })();
